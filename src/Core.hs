28fc943a62edac5a4a29c4801cb27aaad158a25c
Christopher Gillis <christopher.l.gillis@gmail.com>
2013-10-09 14:29:43 +0000

    Added Core.hs - Statskell.Core Includes StandardDeviation, Variance, and Mean Needs more testing

---
+++ src/Core.hs
@@ -0,0 +1,24 @@
+module Statskell.Core
+( standardDeviation
+, variance
+, mean
+) where
+
+import Data.List
+
+-- Public Functions
+
+standardDeviation :: (Floating a) => [a] -> a
+standardDeviation [] = 0
+standardDeviation xs = sqrt $ variance xs 
+
+variance :: (Fractional a) => [a] -> a
+variance [] = 0
+variance xs = (1/genericLength xs) * (sum $ map subtractAndSquare xs)
+    where subtractAndSquare x = (x - listMean) ^ 2 
+          listMean = mean xs
+          
+
+mean :: (Fractional a) => [a] -> a
+mean [] = 0
+mean xs = sum xs / genericLength xs 
